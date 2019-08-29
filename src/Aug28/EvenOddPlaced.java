d2489d516d345c8fa393356a66dacf1922b6d6df
frankenstein32 <saifirashid32@gmail.com>
2019-08-29 10:07:57 +0530

    Doubt-Class Codes

---
+++ src/Aug28/EvenOddPlaced.java
@@ -0,0 +1,38 @@
+package Aug28;
+
+import java.util.Scanner;
+
+public class EvenOddPlaced {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+
+		int sum_even = 0;
+		int sum_odd = 0;
+
+		int place = 1;
+
+		while (n != 0) {
+
+			int rem = n % 10;
+
+			if (place % 2 == 0) {
+				sum_even = sum_even + rem;
+			} else {
+				sum_odd = sum_odd + rem;
+			}
+
+			n = n / 10;
+			place++;
+
+		}
+
+		System.out.println(sum_odd);
+		System.out.println(sum_even);
+
+	}
+
+}
---
+++ src/Aug28/doubleSided.java
@@ -0,0 +1,80 @@
+package Aug28;
+
+import java.util.Scanner;
+
+public class doubleSided {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+
+		int n = scn.nextInt();
+		int nst = 1;
+		int nsp1 = n - 1;
+		int nsp2 = -1;
+
+		int rows = 1;
+
+		while (rows <= n) {
+
+			int val;
+			if (rows <= n / 2 + 1) {
+				val = rows;
+			} else {
+				val = n - rows + 1;
+			}
+
+			int csp1 = 1;
+			while (csp1 <= nsp1) {
+
+				System.out.print("  ");
+				csp1++;
+			}
+
+			int cst = 1;
+			while (cst <= nst) {
+				System.out.print(val + " ");
+				val--;
+				cst++;
+			}
+
+			int csp2 = 1;
+			while (csp2 <= nsp2) {
+
+				System.out.print("  ");
+				csp2++;
+			}
+
+			cst = 1;
+
+			if (rows == 1 || rows == n) {
+				cst = 2;
+			}
+
+			val++;
+			while (cst <= nst) {
+				System.out.print(val + " ");
+				val++;
+				cst++;
+			}
+
+			if (rows <= n / 2) {
+
+				nst++;
+				nsp1 -= 2;
+				nsp2 += 2;
+			} else {
+
+				nst--;
+				nsp1 += 2;
+				nsp2 -= 2;
+			}
+
+			rows++;
+			System.out.println();
+
+		}
+
+	}
+
+}
